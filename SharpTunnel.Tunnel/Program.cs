acdcbb6221f56164537689c283ad1ffb16e25119
rthomasv3 <39268276+rthomasv3@users.noreply.github.com>
2024-09-12 14:58:33 -0500

    reorganize projects

---
+++ SharpTunnel.Tunnel/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SharpTunnel.Tunnel;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
